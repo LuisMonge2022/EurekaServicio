122ef2742900880b935c24978ea1302c08cff78d
LuisMonge2022 <luisyangelo2011@gmail.com>
2022-08-11 21:36:39 -0500

    listo

---
+++ src/main/java/idat/com/EurekaServicio/EurekaServicioApplication.java
@@ -0,0 +1,15 @@
+package idat.com.EurekaServicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServicioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/idat/com/EurekaServicio/EurekaServicioApplicationTests.java
@@ -0,0 +1,13 @@
+package idat.com.EurekaServicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
